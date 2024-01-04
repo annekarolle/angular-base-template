6b2ef0315e3641dfa433b7b1f8ff0a735afc1d3d
annekarolle <102996739+annekarolle@users.noreply.github.com>
2024-01-04 18:18:58 -0300

    adicionado novas features usando documentação do angular 17

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,9 +4,38 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   standalone: true,
   imports: [],
-  templateUrl: './footer.component.html',
+  template: `
+  <footer>
+      <ul>
+      <li>
+          <span>
+
+          {{informacoesLocal.endereco}}
+        </span>
+      </li>
+      <li>
+          <span>
+
+          {{informacoesLocal.telefone}}  | {{informacoesLocal.email}}
+        </span>
+      </li>
+      <li>
+          <span>
+
+          {{informacoesLocal.horarioFuncionamento}}
+        </span>
+      </li>
+      </ul>
+  </footer>
+  `,
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  informacoesLocal = {
+    endereco: '123 Rua Fictícia, Quadra 456 - Lote 789 - Bairro Imaginário, Cidade dos Sonhos - XX, 12345-678',
+    telefone: '(00) 1234-5678',
+    email: 'contato@exemplo.com',
+    horarioFuncionamento: 'Segunda a Sexta-feira das 9h às 18h',
+    sugestaoNovosHorarios: 'Sugerir novos horários através do e-mail sugestoes@exemplo.com',
+  };
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../components/header/header.component";
 import {FooterComponent} from "../../components/footer/footer.component";
+import {ImagensComponent} from "../../components/imagens/imagens.component";
+import {FormComponent} from "../../components/form/form.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ImagensComponent, FormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
